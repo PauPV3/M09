dad0bd00b613f5d75eed2ca94a5a489e560cecc5
Pau Pich <pichvives3@gmail.com>
2024-10-16 20:54:38 +0200

    Activitat 5

---
+++ Activitat 5/AES.java
@@ -0,0 +1,21 @@
+public class AES {
+    public static final String ALGORISME_XIFRAT = "AES";
+    public static final String ALGORISME_HAH = "SHA-256";
+    public static final String FORMAT_AES = "AES/CBC/PKCS5Padding";
+
+    private static final int MIDA_IV = 16;
+    private static byte[] iv = new byte[MIDA_IV];
+    private static final String CLAU = "ElFCBGuanyaraElTriplet";
+
+    public static byte[] xifraAES(String msg, String clau) throws Exception {
+
+    }
+    
+    public static String desxifraAES(byte[] bMsgXifrat, String clau) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
